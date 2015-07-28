565d048c949571ed437b42ae927ab06ad3560643
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2015-07-28 22:38:53 +0200

    Fixed author email

--- syntax/glyphicon.php
+++ syntax/glyphicon.php
@@ -3,7 +3,7 @@
  * Plugin Icons: Glyphicons helper
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi>
+ * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
  * @copyright  (C) 2015, Giuseppe Di Terlizzi
  */
  
