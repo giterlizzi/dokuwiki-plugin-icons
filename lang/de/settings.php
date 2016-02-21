011625f6e63726ffd557e0c3ab8ec669f8d251a3
Dominik <dominik@reichardt-online.it>
2016-02-21 12:33:11 +0100

    Create settings.php

---
+++ lang/de/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * German language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @author     Dominik Reichardt <dominik@reichardt-online.it>
+ */
+ 
+// for the configuration manager
+$lang['defaultSize']     = 'Standard Größe der Icons';
+$lang['defaultPack']     = 'Default icon package';
+$lang['fontAwesomeURL']  = 'Font Awesome CSS URL (CDN oder local)';
+$lang['fugueURL']        = 'Fugue Basis Pfad URL (CDN oder local)';
+$lang['oxygenURL']       = 'Oxygen Basis Pfad URL (CDN oder local)';
+$lang['loadFontAwesome'] = 'Soll die css Datei von Font Awesome geladen werden?';
+$lang['silkURL']         = 'Silk Basis Pfad URL (CDN or local)';
+$lang['flagURL']         = 'Flag Basis Pfad URL (CDN or local)';
