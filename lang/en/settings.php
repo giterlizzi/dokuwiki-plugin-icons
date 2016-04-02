83c53bf5ab2707213e9dbd97547f420491498c26
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2016-04-02 23:43:53 +0200

    Added missing translation for Typicons (#11)

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,13 +5,15 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
  */
- 
+
 // for the configuration manager
 $lang['defaultSize']     = 'Default size for icon';
 $lang['defaultPack']     = 'Default icon package';
 $lang['fontAwesomeURL']  = 'Font Awesome CSS URL (CDN or local)';
+$lang['typiconsURL']     = 'Typicons CSS URL (CDN or local)';
 $lang['fugueURL']        = 'Fugue base path URL (CDN or local)';
 $lang['oxygenURL']       = 'Oxygen base path URL (CDN or local)';
 $lang['loadFontAwesome'] = 'Do you want load the CSS file of Font Awesome ?';
+$lang['loadTypicons']    = 'Do you want load the CSS file of Typicons ?';
 $lang['silkURL']         = 'Silk base path URL (CDN or local)';
 $lang['flagURL']         = 'Flag base path URL (CDN or local)';
