5957d86bdf0d584d584af650423fc42629c4e122
AlekSet <toolman@magazynek.org>
2016-04-14 20:01:25 +0200

    Polish language

---
+++ lang/pl/settings.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Polish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @author     Aleksander Setlak
+ */
+
+// for the configuration manager
+$lang['defaultSize']     = 'Domyślny rozmiar ikon';
+$lang['defaultPack']     = 'Domyślna paczka ikon';
+$lang['fontAwesomeURL']  = 'Font Awesome - adres pliku CSS (CDN lub lokalny)';
+$lang['typiconsURL']     = 'Typicons - adres pliku CSS (CDN lub lokalny)';
+$lang['fugueURL']        = 'Fugue - adres bazowy (CDN lub lokalny)';
+$lang['oxygenURL']       = 'Oxygen - adres bazowy (CDN lub lokalny)';
+$lang['loadFontAwesome'] = 'Czy chcesz załadować plik CSS Font Awesome?';
+$lang['loadTypicons']    = 'Czy chcesz załadować plik CSS Typicons?';
+$lang['silkURL']         = 'Silk - adres bazowy (CDN lub lokalny)';
+$lang['flagURL']         = 'Flag - adres bazowy (CDN lub lokalny)';
