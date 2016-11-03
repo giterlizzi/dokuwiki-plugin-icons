d3e42b6d71dbbe6fcf18fef877e9defef15807d0
ReadWriteGeo <ReadWriteGeo@tg35.com>
2016-11-03 16:18:47 +0100

    Add files via upload
    According to "stephenhutchings", the set of icons is named typicons.
    https://github.com/stephenhutchings/typicons.font

--- conf/default.php
+++ conf/default.php
@@ -7,10 +7,10 @@
 
 $conf['defaultSize']     = '16';
 $conf['defaultPack']     = 'fa';
-$conf['fontAwesomeURL']  = '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css';
+$conf['fontAwesomeURL']  = 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css';
 $conf['loadFontAwesome'] = 1;
-$conf['typiconURL']      = '//cdn.rawgit.com/stephenhutchings/typicons.font/master/src/font/typicons.min.css';
-$conf['loadTypicon']     = 1;
+$conf['typiconsURL']     = 'https://cdn.rawgit.com/stephenhutchings/typicons.font/master/src/font/typicons.min.css';
+$conf['loadTypicons']    = 1;
 $conf['fugueURL']        = 'https://raw.githubusercontent.com/yusukekamiyamane/fugue-icons/master';
 $conf['oxygenURL']       = 'https://raw.githubusercontent.com/pasnox/oxygen-icons-png/master/oxygen';
 $conf['silkURL']         = 'http://icons.iconarchive.com/icons/famfamfam/silk/16';
--- conf/metadata.php
+++ conf/metadata.php
@@ -10,6 +10,8 @@ $meta['defaultSize']     = array('string');
 $meta['defaultPack']     = array('multichoice','_choices' => array('fa', 'glyphicon', 'fugue', 'oxygen'));
 $meta['loadFontAwesome'] = array('onoff');
 $meta['fontAwesomeURL']  = array('string');
+$meta['loadTypicons']    = array('onoff');
+$meta['typiconsURL']     = array('string');
 $meta['fugueURL']        = array('string');
 $meta['oxygenURL']       = array('string');
 $meta['silkURL']         = array('string');
