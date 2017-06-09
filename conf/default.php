8aaee3af28539db6a51237de3f1193181192f8b3
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2017-06-09 11:31:51 +0200

    Fix URL

--- conf/default.php
+++ conf/default.php
@@ -7,11 +7,11 @@
 
 $conf['defaultSize']     = '16';
 $conf['defaultPack']     = 'fa';
-$conf['fontAwesomeURL']  = 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css';
+$conf['fontAwesomeURL']  = '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css';
 $conf['loadFontAwesome'] = 1;
-$conf['typiconsURL']     = 'https://cdn.rawgit.com/stephenhutchings/typicons.font/master/src/font/typicons.min.css';
+$conf['typiconsURL']     = '//cdn.rawgit.com/stephenhutchings/typicons.font/master/src/font/typicons.min.css';
 $conf['loadTypicons']    = 1;
-$conf['fugueURL']        = 'https://raw.githubusercontent.com/yusukekamiyamane/fugue-icons/master';
-$conf['oxygenURL']       = 'https://raw.githubusercontent.com/pasnox/oxygen-icons-png/master/oxygen';
-$conf['silkURL']         = 'http://icons.iconarchive.com/icons/famfamfam/silk/16';
-$conf['flagURL']         = 'http://icons.iconarchive.com/icons/famfamfam/flag/16';
+$conf['fugueURL']        = '//raw.githubusercontent.com/yusukekamiyamane/fugue-icons/master';
+$conf['oxygenURL']       = '//raw.githubusercontent.com/pasnox/oxygen-icons-png/master/oxygen';
+$conf['silkURL']         = '//icons.iconarchive.com/icons/famfamfam/silk/16';
+$conf['flagURL']         = '//icons.iconarchive.com/icons/famfamfam/flag/16';
