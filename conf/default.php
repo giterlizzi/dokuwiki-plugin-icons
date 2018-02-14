91fab7447888784c6b56a767c1571e8d4012d32c
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2018-02-14 11:08:11 +0100

    Switcher to https for all icons and set FontAwesome to v4.7.0 (thanks to @MvErven on #16)

--- conf/default.php
+++ conf/default.php
@@ -8,16 +8,16 @@
 $conf['defaultSize']     = '16';
 $conf['defaultPack']     = 'fa';
 
-$conf['fontAwesomeURL']  = '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css';
+$conf['fontAwesomeURL']  = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
 $conf['loadFontAwesome'] = 1;
 
-$conf['typiconsURL']     = '//cdn.rawgit.com/stephenhutchings/typicons.font/master/src/font/typicons.min.css';
-$conf['loadTypicons']    = 1;
+$conf['typiconsURL']     = 'https://cdn.rawgit.com/stephenhutchings/typicons.font/master/src/font/typicons.min.css';
+$conf['loadTypicons']    = 0;
 
-$conf['fontlinuxURL']    = '//cdn.rawgit.com/Lukas-W/font-linux/v0.9/assets/font-linux.css';
-$conf['loadFontlinux']   = 1;
+$conf['fontlinuxURL']    = 'https://cdn.rawgit.com/Lukas-W/font-linux/v0.9/assets/font-linux.css';
+$conf['loadFontlinux']   = 0;
 
-$conf['fugueURL']        = '//raw.githubusercontent.com/yusukekamiyamane/fugue-icons/master';
-$conf['oxygenURL']       = '//raw.githubusercontent.com/pasnox/oxygen-icons-png/master/oxygen';
+$conf['fugueURL']        = 'https://raw.githubusercontent.com/yusukekamiyamane/fugue-icons/master';
+$conf['oxygenURL']       = 'https://raw.githubusercontent.com/pasnox/oxygen-icons-png/master/oxygen';
 $conf['silkURL']         = 'http://icons.iconarchive.com/icons/famfamfam/silk/16';
 $conf['flagURL']         = 'http://icons.iconarchive.com/icons/famfamfam/flag/16';
